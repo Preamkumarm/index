ccdebbcd43e8830ce6c70dac547958bf30319d20
preamkumarm <preamkumarmano@gmail.com>
2024-02-07 19:49:45 +0530

    pream

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+function one(event)
+{
+ var popupoverlay=document.querySelector(".popup-overlay")
+ var popupbox=document.querySelector(".popup-box")
+var addpopupbutton=document.getElementById("add-popup-button")
+
+ addpopupbutton.addEventListener("click",function(){
+
+ 
+ popupoverlay.style.display="block"
+ popupbox.style.display="block"
+})
+  
+  
 }
-
-export default App;
+  return(<div>
+     <h1 className="navbar">BookSky</h1>
+     <div className="container">
+      <div className="book-container">
+        <h2>Rich Dad Poor Dad</h2>
+        <h5>Robert</h5>
+        <p>Rich Dad Poor Dad is a 1997 book written by Robert T. Kiyosaki and Sharon Lechter. It advocates the importance of financial literacy, financial independence and building wealth through investing in assets, real estate investing, starting and owning businesses, as well as increasing one's financial intelligence</p>
+        <button>Delete</button>
+       <button className="makeup">Edit</button>
+      </div>
+      <div className="popup-overlay">
+        <div className="popup-box">
+           <h2 className="hook">Add Book</h2>
+           <form>
+            <input type="Text" placeholder="Book Title"></input>
+            <input type="text" placeholder="Book Author"></input>
+            <textarea placeholder="Short Description" id="book-description"></textarea>
+            <button className="add-book">ADD</button>
+            <button className="cancel-popup">CANCEL</button>
+           </form>
+        </div>
+       
+      </div>
+     </div>
+     <button className="add-button" id="add-popup-button" onClick={one()}>+</button>
+  </div>)
+}
+export default App
